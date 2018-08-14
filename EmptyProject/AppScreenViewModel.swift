b91775b5939f37fdc9698b498f3c9928c5551093
Erick Martin <osdudutz@yahoo.com>
2018-08-14 17:56:44 +0700

    added appscreenviewmodel

---
+++ EmptyProject/AppScreenViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  AppScreenViewModel.swift
+//
+//
+//  Created by Erick Martin on 9/8/16.
+//
+//
+
+import Foundation
+import RxSwift
+
+class AppScreenViewModel {
+    
+    init() {}
+    
+    func doForceLogout() {
+
+    }
+}
